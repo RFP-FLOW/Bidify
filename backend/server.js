afe11f74d11045fd8fb30583baa943914f52ffdf
hg2411 <himanigoyal764@gmail.com>
2026-01-21 00:48:36 +0530

    created login backend

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,32 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
 const app = express();
+
+// DB
 connectDB();
 
-const PORT = process.env.PORT || 7000;
+// Middlewares
+app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Bidify API running");
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 API Server running on port ${PORT}`);
 });
