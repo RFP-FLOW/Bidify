e3b0dc383b39160ce6464dd2c9e91471b667a890
Anshika <anshikakesri02@gmail.com>
2026-07-10 16:25:27 +0530

    Server.js CORS changed

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ await connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
